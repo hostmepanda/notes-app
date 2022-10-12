92e102ab6ddb2c8e7b096cffa9fdb863f6d156cf
hostmepanda <noah.vite@gmail.com>
2022-10-12 17:09:05 +0300

    Add styles for button

--- App.js
+++ App.js
@@ -1,26 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.caption}>Super Notes App</Text>
-      <View style={styles.horizontalLine} />
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 const styles = StyleSheet.create({
+  addButtonWrapper: {
+    display: 'flex',
+    position: 'absolute',
+    bottom: 50,
+    width: 50,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  addButton: {
+    fontSize: '70px',
+    color: '#5a2a81',
+    top: -25,
+    height: 100,
+  },
+  addButtonCircle: {
+    display: 'flex',
+    alignItems: '',
+    borderWidth: 3,
+    borderStyle: 'solid',
+    borderRadius: '100',
+    borderColor: '#5a2a81',
+    height: 50,
+    width: 50,
+  },
+  caption: {
+    fontSize: '20px',
+  },
   container: {
-    paddingTop: 60,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'flex-start',
-  },
-  caption: {
-    fontSize: '20px',
+    paddingTop: 60,
   },
   horizontalLine: {
     marginTop: 10,
@@ -29,3 +44,19 @@ const styles = StyleSheet.create({
     width: '70%',
   },
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.caption}>Super Notes App</Text>
+      <View style={styles.horizontalLine} />
+      <StatusBar style="auto"/>
+      <View style={styles.addButtonWrapper}>
+        <TouchableOpacity style={styles.addButtonCircle}>
+          <Text style={styles.addButton}>+</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
