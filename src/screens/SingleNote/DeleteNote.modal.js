4fa2ea8b2d454aacd88ff4ff060976d09bcb1425
hostmepanda <noah.vite@gmail.com>
2022-10-16 09:33:18 +0300

    Refactor styles of delete modal

--- src/screens/SingleNote/DeleteNote.modal.js
+++ src/screens/SingleNote/DeleteNote.modal.js
@@ -15,39 +15,14 @@ export const DeleteNoteModal = () => {
         onRequestClose={() => setIsModalVisible(false)}
       >
         <View style={styles.modalFrame}>
-          <View style={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '100%',
-          }}>
-            <Text
-              style={{
-                height: '50%',
-                textAlign: 'center',
-              }}
-            >Do you want to delete the note?</Text>
-            <View style={{
-              width: '80%',
-              borderColor: 'rgba(70,70,70,0.26)',
-              borderWidth: 0.5,
-            }} />
-            <View style={{
-              paddingTop: 10,
-              width: '100%',
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'space-evenly',
-            }}>
+          <View style={styles.modalBody}>
+            <Text style={styles.modalText}>Do you want to delete the note?</Text>
+            <View style={styles.modalBodyHorizontalLine} />
+            <View style={styles.modalFooter}>
               <TouchableOpacity>
-                <Text style={{ color: 'red' }}>Delete</Text>
+                <Text style={styles.modalFooterLeftButton}>Delete</Text>
               </TouchableOpacity>
-              <View style={{
-                width: 1,
-                borderColor: 'rgba(70,70,70,0.49)',
-                borderWidth: 0.5,
-              }} />
+              <View style={styles.modalFooterRightButton} />
               <TouchableOpacity onPress={() => setIsModalVisible(false)}>
                 <Text>Keep it!</Text>
               </TouchableOpacity>
@@ -56,7 +31,7 @@ export const DeleteNoteModal = () => {
         </View>
       </Modal>
       <Pressable onPress={() => setIsModalVisible(true)}>
-        <Text style={{ color: 'red' }}>Delete</Text>
+        <Text style={styles.modalOpenButton}>Delete</Text>
       </Pressable>
     </View>
   );
--- src/screens/SingleNote/DeleteNote.modal.style.js
+++ src/screens/SingleNote/DeleteNote.modal.style.js
@@ -11,4 +11,34 @@ export const deleteNoteModalStyle = StyleSheet.create({
     top: '50%',
     marginTop: -100,
   },
+  modalBody: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+  },
+  modalBodyHorizontalLine: {
+    width: '80%',
+    borderColor: 'rgba(70,70,70,0.26)',
+    borderWidth: 0.5,
+  },
+  modalOpenButton: { color: 'red' },
+  modalFooter: {
+    paddingTop: 10,
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
+  modalFooterLeftButton: { color: 'red' },
+  modalFooterRightButton: {
+    width: 1,
+    borderColor: 'rgba(70,70,70,0.49)',
+    borderWidth: 0.5,
+  },
+  modalText: {
+    height: '50%',
+    textAlign: 'center',
+  },
 });
