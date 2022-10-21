726d339a49172885f3fa741902df1b16abad91bb
hostmepanda <noah.vite@gmail.com>
2022-10-21 13:15:06 +0300

    Refactor NoteState reducer

--- src/context/reducers/NotesState.reducer.js
+++ src/context/reducers/NotesState.reducer.js
@@ -2,50 +2,52 @@ import { ActionTypes } from '../ActionTypes';
 
 const NOT_FOUND = -1;
 
-export const NotesStateReducer = (state, { type, payload }) => {
-  if (!payload?.id) {
-    return state;
-  }
+const actionHandlers = {
+  [ActionTypes.addNote]: (state, payload) => {
+    return {
+      ...state,
+      notes: [
+        ...state.notes,
+        {
+          id: payload?.id ?? (new Date()).valueOf(),
+          title: payload?.title ?? 'New amazing note',
+          content: payload?.content ?? '',
+        },
+      ],
+    };
+  },
+  [ActionTypes.removeNote]: (state, payload) => {
+    return {
+      ...state,
+      notes: state.notes.filter(note => note.id !== payload.id),
+    };
+  },
+  [ActionTypes.updateNote]: (state, payload) => {
+    const updateNoteIndex = state.notes.findIndex(({ id  }) => id === payload.id);
 
-  switch (type) {
-    case ActionTypes.addNote: {
+    if (updateNoteIndex !== NOT_FOUND) {
+      const updateNote = state.notes[updateNoteIndex];
+      state.notes[updateNoteIndex] = {
+        ...updateNote,
+        title: payload.title ?? updateNote?.title,
+        content: payload.content ?? updateNote?.content,
+      };
       return {
         ...state,
         notes: [
           ...state.notes,
-          {
-            id: payload?.id ?? (new Date()).valueOf(),
-            title: payload?.title ?? 'New amazing note',
-            content: payload?.content ?? '',
-          },
         ],
       };
     }
-    case ActionTypes.removeNote: {
-      return {
-        ...state,
-        notes: state.notes.filter(note => note.id !== payload.id),
-      };
-    }
-    case ActionTypes.updateNote: {
-      const updateNoteIndex = state.notes.findIndex(({ id  }) => id === payload.id);
+    return state;
+  },
+  DEFAULT: (state) => state,
+};
 
-      if (updateNoteIndex !== NOT_FOUND) {
-        const updateNote = state.notes[updateNoteIndex];
-        state.notes[updateNoteIndex] = {
-          ...updateNote,
-          title: payload.title ?? updateNote?.title,
-          content: payload.content ?? updateNote?.content,
-        };
-        return {
-          ...state,
-          notes: [
-            ...state.notes,
-          ],
-        };
-      }
-      return state;
-    }
-    default: return state;
+export const NotesStateReducer = (state, { type, payload }) => {
+  if (!payload?.id) {
+    return state;
   }
+  const actionHandler = actionHandlers?.[type] ?? actionHandlers.DEFAULT;
+  actionHandler(state, payload);
  };
